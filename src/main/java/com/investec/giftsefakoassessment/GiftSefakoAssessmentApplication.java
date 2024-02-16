363c04f8df7e8b603bd856495ce0ff69ccef33db
giftsefako <gift.sefako@momentum.co.za>
2024-02-16 12:19:29 +0200

    init commit - springboot project

---
+++ src/main/java/com/investec/giftsefakoassessment/GiftSefakoAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.investec.giftsefakoassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftSefakoAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftSefakoAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investec/giftsefakoassessment/GiftSefakoAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investec.giftsefakoassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftSefakoAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
